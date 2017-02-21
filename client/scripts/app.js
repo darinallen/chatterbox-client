f77b1aec270abc57f9d0ba424bdf59b09b9e9da3
Darin Allen <darinallen@gmail.com>
2017-02-20 23:34:41 -0500

    Fetch function implemented, and is delivering success message, though test not passing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,6 +25,32 @@ app.send = function(message)  {
   });
 };
 
+app.fetch = function() {
+  $.get('http://parse.hrr.hackreactor.com/chatterbox/classes/messages',function(messages) {
+    console.log('messages came in');
+    console.log(messages);
+    var messages = messages.results;
+    messages.forEach(function(message) {
+      // var text = jsesc(message.text);
+      $('#chats').append('<div>' + message.text + '</div>');
+    });
+  });
+
+  // $.ajax({
+  // // This is the url you should use to communicate with the parse API server.
+  //   url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+  //   type: 'GET',
+  //   // contentType: 'application/json',
+  //   success: function (data) {
+  //     console.log('chatterbox: Messages received');
+  //   },
+  //   error: function (data) {
+  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //     console.error('chatterbox: Failed to receive messages', data);
+  //   }
+  // })
+};
+
 $(document).ready(function() {
   $( ".btn" ).click(function(event) {
     var visitorMessage = $( ".entry" ).val();
@@ -37,3 +63,5 @@ $(document).ready(function() {
     app.send(message);
   });
 });
+
+// app.fetch();
